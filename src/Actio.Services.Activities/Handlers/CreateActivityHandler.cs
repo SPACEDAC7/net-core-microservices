1bde752de44a44a387f20ddd2c983f0f9c4e744f
David Albalate Cabedo <capelo39@gmail.com>
2020-05-15 07:17:58 +0100

    Subscribing messages part 2

---
+++ src/Actio.Services.Activities/Handlers/CreateActivityHandler.cs
@@ -0,0 +1,29 @@
+﻿using Actio.Common.Commands;
+using Actio.Common.Events;
+using RawRabbit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Actio.Services.Identity.Handlers
+{
+    public class CreateActivityHandler : ICommandHandler<CreateActivity>
+    {
+
+        private readonly IBusClient busClient;
+
+        public CreateActivityHandler(IBusClient busClient)
+        {
+            this.busClient = busClient;
+        }
+
+        public async Task HandleAsync(CreateActivity command)
+        {
+            Console.WriteLine($"Creating activity: {command.Name}");
+            await this.busClient.PublishAsync(new ActivityCreated(command.Id,
+                command.UserId, command.Category, command.Name));
+            throw new NotImplementedException();
+        }
+    }
+}
