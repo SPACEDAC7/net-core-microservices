a9d4c6cb32dc2bd990b88c50529c8d7391b70376
David Albalate Cabedo <capelo39@gmail.com>
2020-05-19 07:46:24 +0100

    Visual studio needs autosave

--- src/Actio.Services.Activities/Domain/Repositories/ICategoryRepository.cs
+++ src/Actio.Services.Activities/Domain/Repositories/ICategoryRepository.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿using Actio.Services.Activities.Domain.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Actio.Services.Activities.Domain.Repositories
 {
-    interface ICategoryRepository
+    public interface ICategoryRepository
     {
+        Task<Category> GetAsync(string name);
+        Task<IEnumerable<Category>> BrowseAsync();
+        Task AddAsync(Category category);
     }
 }
